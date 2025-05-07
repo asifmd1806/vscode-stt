3bfd12399a45a81789987217fdf608d905e8ca65
Asif <asif18061996@gmail.com>
2025-05-07 20:07:23 +0530

    feat: Improve native module handling and release workflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const AssetRelocatorLoader = require('@vercel/webpack-asset-relocator-loader');
 
 module.exports = {
   target: 'node',
@@ -22,8 +23,40 @@ module.exports = {
       {
         test: /\.ts$/,
         exclude: /node_modules/,
-        use: [{ loader: 'ts-loader' }],
+        use: [
+          {
+            loader: AssetRelocatorLoader.loader,
+            options: {
+              // Options for the asset relocator loader if needed
+              // By default, it will emit files to the output path (dist)
+              // and adjust paths in the code.
+              // 'outputAssetBase': 'native_modules' // To put them in dist/native_modules
+            },
+          },
+          { loader: 'ts-loader' },
+        ],
+      },
+      // You might need a similar rule for .js files if your dependencies have them
+      // and they also require native modules.
+      {
+        test: /\.js$/,
+        exclude: /node_modules\/(?!some-module-that-needs-processing)/, // exclude node_modules generally
+        use: [
+          {
+            loader: AssetRelocatorLoader.loader,
+          },
+          // Add other loaders for JS if needed, e.g., babel-loader
+        ],
+      },
+      // Rule for .node files (though AssetRelocatorLoader should handle them)
+      {
+        test: /\.node$/,
+        use: 'node-loader',
       },
     ],
   },
+  // Optional: If you want to see more logs from the relocator
+  // stats: {
+  //   logging: 'verbose',
+  // },
 }; 
